f0774e2bc6bb90a27e860ffd18ae44156f695381
justinnonso05 <chinonsoali2005@gmail.com>
2024-07-09 08:57:48 -0700

    settings

--- review/settings.py
+++ review/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'assets'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
